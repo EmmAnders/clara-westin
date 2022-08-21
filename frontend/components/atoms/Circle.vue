0bb32b47e5d5056f867e9b94e335749ca37a033d
Emma Andersson <73166517+EmmAnders@users.noreply.github.com>
2022-08-21 09:12:31 +0200

    add circle

---
+++ frontend/components/atoms/Circle.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+    class="a-circle shadow-sm w-[70px] h-[70px] border-thin border-theme-color bg-cream-white rounded-full flex justify-center items-center"
+  >
+    <span> <slot /></span>
+  </div>
+</template>
+<style lang="scss"></style>
