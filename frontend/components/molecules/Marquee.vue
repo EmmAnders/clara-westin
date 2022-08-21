b66ef5277e6de363f7f520c25c51b905a235f73f
Emma Andersson <73166517+EmmAnders@users.noreply.github.com>
2022-08-21 13:29:20 +0200

    add markup marquee

---
+++ frontend/components/molecules/Marquee.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="border-y-thin border-theme-color -mx-8 px-8 py-4 text-center">
+    <p class="text-xl">
+      Vero eos et accusamus et iusto odio Vero eos et accusamus et iusto odio
+      Vero eos et accusamus et iusto odio Vero eos et accusamus et.
+    </p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  content: {
+    type: Array,
+    required: false,
+  },
+});
+</script>
