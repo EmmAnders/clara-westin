e926134629a16744e6af21cc522e543aa1f7b991
Emma Andersson <73166517+EmmAnders@users.noreply.github.com>
2022-08-21 13:28:08 +0200

    add markup header

---
+++ frontend/components/molecules/Navigation.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul class="flex space-x-6">
+    <li
+      class="hover:opacity-hover cursor-pointer"
+      v-for="(item, index) in items"
+      :key="index"
+    >
+      {{ item }}
+    </li>
+  </ul>
+</template>
+
+<script setup>
+const props = defineProps({
+  items: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
---
+++ frontend/components/organisms/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="flex justify-between items-center -mx-8 px-8 py-4">
+    <div class="w-1/2">
+      <AtomsLogo></AtomsLogo>
+    </div>
+    <div class="w-1/2 flex justify-end">
+      <MoleculesNavigation :items="items"></MoleculesNavigation>
+    </div>
+  </header>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const items = ref(["Education hub", "Work with me", "About", "Contact"]);
+</script>
