f781ecb3b8d69931b5fcc278e28beb9e7e826645
Emma Andersson <73166517+EmmAnders@users.noreply.github.com>
2022-08-21 09:12:20 +0200

    add image

---
+++ frontend/components/atoms/Image.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="a-image h-[300px] w-full" :class="radius">
+    <img
+      class="h-full w-full cover"
+      src="https://source.unsplash.com/random"
+      alt=""
+    />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  radius: {
+    type: String,
+    default: "top-left",
+    required: false,
+  },
+});
+</script>
+
+<style lang="scss">
+.a-image {
+  img {
+    @apply rounded-md;
+    box-shadow: 0 0 0 0.5px currentColor;
+  }
+
+  &.top-left {
+    img {
+      @apply rounded-tl-[80px];
+    }
+  }
+  &.top-right {
+    img {
+      @apply rounded-tr-[80px];
+    }
+  }
+
+  &.bottom-left {
+    img {
+      @apply rounded-bl-[80px];
+    }
+  }
+
+  &.bottom-right {
+    img {
+      @apply rounded-br-[80px];
+    }
+  }
+}
+</style>
