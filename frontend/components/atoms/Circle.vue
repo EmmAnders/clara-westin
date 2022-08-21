968d19199020ac5fbc28b546f7d326e8bb331db7
Emma Andersson <73166517+EmmAnders@users.noreply.github.com>
2022-08-21 13:28:49 +0200

    minor changes size

--- frontend/components/atoms/Circle.vue
+++ frontend/components/atoms/Circle.vue
@@ -1,8 +1,8 @@
 <template>
   <div
-    class="a-circle shadow-sm w-[70px] h-[70px] border-thin border-theme-color bg-cream-white rounded-full flex justify-center items-center"
+    class="a-circle shadow-sm w-[70px] h-[70px] lg:w-[90px] lg:h-[90px] border-thin border-theme-color bg-cream-white rounded-full flex justify-center items-center"
   >
-    <span> <slot /></span>
+    <span class="font-secondary text-md"> <slot /></span>
   </div>
 </template>
 <style lang="scss"></style>
