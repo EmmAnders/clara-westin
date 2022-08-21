1122d760a2b4036277c4ff2cc28148d8ac313eea
Emma Andersson <73166517+EmmAnders@users.noreply.github.com>
2022-08-21 09:12:13 +0200

    add pill

---
+++ frontend/components/atoms/Pill.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="a-pill inline-flex px-4 py-1.5 rounded-full shadow-sm"
+    :class="type"
+  >
+    <slot />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  type: {
+    type: String,
+    default: "outlined",
+    required: false,
+  },
+});
+</script>
+
+<style lang="scss">
+.a-pill {
+  &.outlined {
+    @apply border-thin border-theme-color;
+  }
+
+  &.contained {
+    @apply bg-blue;
+  }
+}
+</style>
